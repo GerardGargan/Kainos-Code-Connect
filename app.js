fc445803d597b283a27dcb660f8fff05787ff921
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-01-25 11:38:00 +0000

    set up app, router and db connection

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./routes/routes');
+const PORT = 3000;
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.set('view engine', 'ejs');
+app.use('/', router);
+
+
+app.listen(PORT, (err) => {
+    if(err){
+        return console.log(err);
+    }
+    console.log(`Server listening on Port ${PORT}`);
+});
