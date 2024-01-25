64f0a03249bb026e69bd9deea545fb4cdcd6e9ce
egertonj21 <egertonjoel@gmail.com>
2024-01-25 14:30:06 +0000

    added add employee functionality

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const morgan = require('morgan');
 const router = require('./routes/routes');
 const PORT = 3000;
 
+
 const app = express();
 
 app.use(morgan('tiny'));
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended:true}))
 app.use('/', router);
 
 
