9b6e0c5952d80cd0f8120b576f087b1706da9bd1
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-01-26 11:04:54 +0000

    added login feature US005 and authenitcation, salary is hidden for normal users

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
 const router = require('./routes/routes');
 const PORT = 3000;
 
 
 const app = express();
 
+app.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 360000}    
+}));
+
 app.use(morgan('tiny'));
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
 app.use('/', router);
 
 
